0d61365c070de6466e0c0c6449395ac301bf40dd
RISHURAJ KUMAR <rishurajkumar01@gmail.com>
2024-07-08 09:43:27 +0530

    calculator.c

--- calculator.c
+++ calculator.c
@@ -1,36 +1,35 @@
+// here is the code which is use of the switch statement and its role as a simple calculator:
+// using switch, case, break
 #include <stdio.h>
 int main() {
     char operator;
-    double num1, num2;
+    float num1, num2;
     
-    // Asking the user to enter the operator
-    printf("Enter an operator (+, -, *, /): ");
+    // asking the user to enter the operator
+    printf("Enter an operator (+, -, *, /, %): ");
     scanf("%c", &operator);
     
-    // Asking the user to enter two operands
+    // asking the user to enter two operands
     printf("Enter two operands: ");
-    scanf("%lf %lf", &num1, &num2);
+    scanf("%f %f", &num1, &num2);
     
     switch (operator) {
         case '+':
-            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            printf("%.2f + %.2f = %.2f\n", num1, num2, num1 + num2);
             break;
         case '-':
-            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            printf("%.2f - %.2f = %.2f\n", num1, num2, num1 - num2);
             break;
         case '*':
-            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            printf("%.2f * %.2f = %.2f\n", num1, num2, num1 * num2);
             break;
         case '/':
             if (num2 != 0)
-                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+                printf("%.2f / %.2f = %.2f\n", num1, num2, num1 / num2);
             else
                 printf("Error! Division by zero.\n");
             break;
         default:
             printf("Error! operator is not correct\n");
     }
-
-    return 0;
 }
-
